355ff5e7e10fd22cd3377bc9e7271d438cdf40b2
buci <anellore@gmail.com>
2013-09-22 22:50:22 -0700

    finalized license info

--- example.py
+++ example.py
@@ -1,3 +1,25 @@
+# example.py: executes simulation of Section 4 from
+# http://cran.r-project.org/web/packages/locfdr/vignettes/locfdr-example.pdf
+#
+# Part of locfdr-python, http://www.github.com/buci/locfdr-python/
+#
+# Copyright (C) 2013 Abhinav Nellore (anellore@gmail.com)
+# Copyright (C) 2011 Bradley Efron, Brit B. Turnbull and Balasubramanian Narasimhan
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License v2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in the file COPYING. If not, write to 
+# the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA 02111-1307 USA
+
 from locfdr import locfdr
 
 f = open('samplez.tsv')
--- setup.py
+++ setup.py
@@ -1,3 +1,22 @@
+# setup.py: for installing locfdr-python
+# Part of locfdr-python, http://www.github.com/buci/locfdr-python/
+#
+# Copyright (C) 2013 Abhinav Nellore (anellore@gmail.com)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License v2 as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details
+#
+# You should have received a copy of the GNU General Public License
+# along with this program in the file COPYING. If not, write to 
+# the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+# Boston, MA 02111-1307 USA
+
 #!/usr/bin/env python
 
 from distutils.core import setup
@@ -5,7 +24,7 @@ from distutils.core import setup
 setup(name = "locfdr-python",
     version = "0.1",
     description = "Computes local false discovery rates.",
-    long_description = "A Python port of R function locfdr(), written by Efron, Turnbull, and Narasimhan.",
+    long_description = "A Python port of R function locfdr(), written by Efron, Turnbull, and Narasimhan and enhanced by Frazee, Collado-Torres, and Leek.",
     author = "Abhinav Nellore",
     author_email = 'anellore@gmail.com',
     url = "http://www.github.com/buci",
